8e24fc70721796638fcec8b003df1d64dac36a20
lujet <462546997@qq.com>
2022-08-09 10:15:00 +0800

    函数作用域

---
+++ func/func.js
@@ -0,0 +1,11 @@
+// 函数作用域和块作用域 
+function foo() {
+	function bar(a) {
+		i = 3; // 修改for循环所属作用域中的i              
+		console.log(a + i);            
+	}            
+	for (var i=0; i<10; i++) {              
+		bar(i * 2); // 糟糕，无限循环了！            
+	}        
+}        
+foo();
